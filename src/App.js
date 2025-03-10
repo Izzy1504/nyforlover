a5eea272fa4fab3eb31380d140db85d339fe8091
Đoàn Minh Khôi <94506631+Izzy1504@users.noreply.github.com>
2025-03-10 22:34:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,17 +10,7 @@ function App() {
     const [startMusic, setStartMusic] = useState(false);
     const slides = [
         {content: "Chúc Mừng Năm Mới 2024! 🎊"},
-        {content: "Bắt đầu một năm mới thật nhiều hạnh phúc và sự trưởng thành sau 1 năm đã qua ✨"},
-        {content: "Sau một năm qua, chúng ta đã trải qua bao nhiêu thăng trầm và mệt mỏi cũng như các lần cãi nhau .🌟"},
-        {content: " Tuy nhiên đó chỉ là những khó khăn mà anh hay em phải gặp trong chuyến hành trình này nèee :3 .Nhờ đó chúng ta cũng trưởng thành hơn, yêu nhau hơn và càng thấu hiểu nhauu nèeee 🏬🌇   "},
-        {content: "Tuy trong hành trình này bản thân anh và em cũng có nhiều sai sót và lỗi lầmm óoooo nhưng mò 🥰🥰🥰"},
-        {content: "Năm mới đến chúng ta cùng nhau xoá bỏ mọi chuyện cũ nhen bé iuu , đừng để những vết thương đó tồn tại sâu trong tâm mà hãy xem nó là những mảnh ghép để đưa anh và em đến cái kết siuu có hậu là đám cưới lunn nạaa :3 💥🎊 💥🎉🎊  "},
-        {content: "Anh mong là chúng ta sẽ có thật nhiều hạnh phúc và đón năm mới tới già cùng nhau nha vợ iuuu :3 🎆 🎇 🎆 🎀"},
-        {content: "Chúc mừng năm với vợ yêu của anhh :3"},
-        {content: "Năm mới anh chúc em bé tuổi mới thật là trưởng thành, vui vẻ hong bị sầu, buồn và đặc biệt phải ở bên anh mãi nheee :3"},
-        {content: "Àaa còn nữa, emm phải học giỏi nè, điểm A thật nhìu và có cơ hội việc làmmm siu cấp đỉnh nóc kịch trần nhenn :3"},
-        {content: "Anhh yêu vợ anh nhất trên đời này luôn ❤ ❣ ☼ ❤️‍🔥 😻  💖 🧧 🎇 , chúc em có một năm mới thật nhiều niềm vui và hạnh phúc nhaaa ❤️‍🔥 🎇 🎆 🎉"},
-        {content: "Vợ yêu của anh ơiiii, anh yêu em nhiều lắm lunn nạaa :3"},
+    
         {content: "MADE BY KHOI MINH DOAN "},
 
 
